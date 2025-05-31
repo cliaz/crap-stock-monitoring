a3f46c651d3f88ca665ec22c53056040513eb86b
cliaz <3636531+cliaz@users.noreply.github.com>
2025-05-31 16:31:36 +1000

    more stuff

---
+++ email_details.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+sender_email = "sender_email_address" 
+sender_password_password = "sender_emaiL_password"  	# app password if sender email address is gmail
+recipients = [
+    "recipient1@gmail.com",
+    "recipient2@gmail.com"
+]
